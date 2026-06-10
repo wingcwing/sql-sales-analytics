02bf21ef3dc5ce5ff479e4a23b1b53ab21c8401d
Wing Wong <143468947+wingcwing@users.noreply.github.com>
2026-06-10 15:23:16 -0400

    Update 04_one_time_vs_repeat_customers.sql

--- scripts/01_customer_analytics/04_one_time_vs_repeat_customers.sql
+++ scripts/01_customer_analytics/04_one_time_vs_repeat_customers.sql
@@ -10,23 +10,21 @@ Business Questions:
     2. What percentage of customers are repeat customers?
 
 ===========================================================================*/
-WITH returning_customer AS(
+WITH customer_order AS(
 SELECT 
     customer_key,
     COUNT(DISTINCT order_number) AS num_orders
 FROM gold.fact_sales
 GROUP BY customer_key)
-, cust_type AS(
+, customer_types AS(
 SELECT
     CASE WHEN num_orders > 1 THEN 'Repeat Customer'
     ELSE 'One-Time Customer'
     END AS customer_type
-FROM returning_customer
+FROM customer_order
 ) SELECT 
     customer_type, 
     COUNT(customer_type) AS num_customers,
     CAST((COUNT(customer_type) * 100.0 / SUM(COUNT(customer_type)) OVER ()) AS DECIMAL(5,2)) AS type_pct
-FROM cust_type
+FROM customer_types
 GROUP BY customer_type
-
-SELECT MAX(order_date) FROM gold.fact_sales
